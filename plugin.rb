4276b723af3491adb87d45142cba604bbdd89029
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-05-16 11:16:27 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,29 +1,22 @@
 # name: discourse-polarsteps-onebox
 # about: Transforme les liens de voyage Polarsteps en cartes interactives (Onebox)
-# version: 1.0
+# version: 1.1
 # authors: Stylevan Club
 # url: https://github.com/votre-compte/discourse-polarsteps-onebox
 
-enabled_site_setting :polarsteps_onebox_enabled
-
-register_asset "../stylesheets/polarsteps-onebox.scss"
+register_asset "stylesheets/polarsteps-onebox.scss"
 
 after_initialize do
-  # On crée un moteur de Onebox personnalisé pour Polarsteps
   class Onebox::Engine::PolarstepsOnebox
     include Onebox::Engine
 
-    # Ce plugin s'active dès qu'un lien correspond à un voyage Polarsteps
-    # Exemple : https://www.polarsteps.com/NomUser/123456-titre-du-voyage
+    # Détection des liens de voyage Polarsteps
     REGEXP = /^https?:\/\/(?:www\.)?polarsteps\.com\/[^\/]+\/([0-9]+)/
-
     matches_regexp REGEXP
 
     def to_html
-      # On extrait l'ID du voyage depuis l'URL
       trip_id = match[1]
       
-      # On génère l'iframe officielle d'intégration de Polarsteps
       <<-HTML
         <div class="polarsteps-onebox-container">
           <iframe 
