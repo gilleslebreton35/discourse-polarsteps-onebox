a145ffbd367be5360073b556e548305d32c86322
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-05-16 11:29:13 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,8 @@
 # name: discourse-polarsteps-onebox
 # about: Transforme les liens de voyage Polarsteps en cartes interactives (Onebox)
-# version: 1.1
+# version: 1.2
 # authors: Stylevan Club
 # url: https://github.com/votre-compte/discourse-polarsteps-onebox
-
-register_asset "stylesheets/polarsteps-onebox.scss"
 
 after_initialize do
   class Onebox::Engine::PolarstepsOnebox
@@ -17,14 +15,15 @@ after_initialize do
     def to_html
       trip_id = match[1]
       
+      # On intègre le style CSS directement en ligne (inline) dans la structure HTML
       <<-HTML
-        <div class="polarsteps-onebox-container">
+        <div class="polarsteps-onebox-container" style="margin: 15px 0; position: relative; width: 100%; max-width: 100%; overflow: hidden; border-radius: 12px; box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);">
           <iframe 
             src="https://www.polarsteps.com/embed/v2/trip/#{trip_id}" 
             width="100%" 
             height="600" 
             frameborder="0" 
-            style="border:0; border-radius:12px; width:100%; height:600px;" 
+            style="border:0; border-radius:12px; width:100%; height:600px; display:block;" 
             allowfullscreen>
           </iframe>
         </div>
