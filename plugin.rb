9089dfe911cf4010e534ece0ffb1f654f4f0a6d8
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-05-16 11:42:12 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-polarsteps-onebox
 # about: Transforme les liens de voyage Polarsteps en cartes interactives (Onebox)
-# version: 1.2
+# version: 1.3
 # authors: Stylevan Club
 # url: https://github.com/votre-compte/discourse-polarsteps-onebox
 
@@ -8,14 +8,14 @@ after_initialize do
   class Onebox::Engine::PolarstepsOnebox
     include Onebox::Engine
 
-    # Détection des liens de voyage Polarsteps
-    REGEXP = /^https?:\/\/(?:www\.)?polarsteps\.com\/[^\/]+\/([0-9]+)/
+    # Cette REGEXP est modifiée pour attraper l'ID même s'il y a un tiret, du texte ou des variables après
+    REGEXP = /^https?:\/\/(?:www\.)?polarsteps\.com\/[^\/]+\/([0-9]+)(?:-[^\/\?]+)?/
     matches_regexp REGEXP
 
     def to_html
+      # On isole proprement l'ID numérique du voyage
       trip_id = match[1]
       
-      # On intègre le style CSS directement en ligne (inline) dans la structure HTML
       <<-HTML
         <div class="polarsteps-onebox-container" style="margin: 15px 0; position: relative; width: 100%; max-width: 100%; overflow: hidden; border-radius: 12px; box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);">
           <iframe 
